0df0052cefa8d955dc8b47db3f88c2a1ae581251
Mohamed Hassan <mohamed>
2022-10-03 19:43:42 +0200

    api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
 //  apiUrl:'https://apis.eltimeseg.com/api/'
-apiUrl:'http://apis.eltimeseg.re-comparison.com/api/'
+apiUrl:'https://apis.eltimeseg.re-comparison.com/api/'
 };
